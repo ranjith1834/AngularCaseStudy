a2ab7b247757fbd40767a66a7924a86d495fb9b8
FNFIS\e1077435 <ranjith.nagavelli@fisglobal.com>
2020-11-30 11:02:00 +0530

    changed email

--- myApp/src/app/employee-list/employee-list.service.ts
+++ myApp/src/app/employee-list/employee-list.service.ts
@@ -8,15 +8,15 @@ import { Employee as Employee } from './employee';
 export class EmployeeListApiService {
   
   employeeList: Array<Employee> = [
-    {id:1001, firstName:"RANJITH", middleName:"", lastName:"Nagavelli", email:"ranjith.nagavelli@fisglobal.com", mobileNumber:"9874563210"},
-    {id:1002, firstName:"User1", middleName:"middleNameOne", lastName:"lastName1", email:"User1.lastname1@fisglobal.com", mobileNumber:"1234567890"},
-    {id:1003, firstName:"User2", middleName:"middleNameTwo", lastName:"lastName2", email:"User2.lastname2@fisglobal.com", mobileNumber:"1236589742"},    
-    {id:1004, firstName:"Ashok", middleName:"", lastName:"Nagavelli2", email:"ranjith2.nagavelli2@fisglobal.com", mobileNumber:"9874563210"},
-    {id:1005, firstName:"vamsi", middleName:"middleNameOne2", lastName:"lastName12", email:"User12.lastname12@fisglobal.com", mobileNumber:"12345678902"},
-    {id:1006, firstName:"Charan", middleName:"middleNameTwo2", lastName:"lastName22", email:"User22.lastname22@fisglobal.com", mobileNumber:"12365897422"},
-    {id:1007, firstName:"RANJITH3", middleName:"", lastName:"Nagavelli3", email:"ranjith3.nagavelli3@fisglobal.com", mobileNumber:"98745632103"},
-    {id:1008, firstName:"Chiru", middleName:"middleNameOne3", lastName:"lastName13", email:"User13.lastname1@fisglobal.com", mobileNumber:"12345678903"},
-    {id:1009, firstName:"Pawan", middleName:"middleNameTwo3", lastName:"lastName23", email:"User23.lastname2@fisglobal.com", mobileNumber:"12365897423"}
+    {id:1001, firstName:"RANJITH", middleName:"", lastName:"Nagavelli", email:"ranjith.nagavelli@gmail.com", mobileNumber:"9874563210"},
+    {id:1002, firstName:"User1", middleName:"middleNameOne", lastName:"lastName1", email:"User1.lastname1@gmail.com", mobileNumber:"1234567890"},
+    {id:1003, firstName:"User2", middleName:"middleNameTwo", lastName:"lastName2", email:"User2.lastname2@gmail.com", mobileNumber:"1236589742"},    
+    {id:1004, firstName:"Ashok", middleName:"", lastName:"Nagavelli2", email:"ranjith2.nagavelli2@gmail.com", mobileNumber:"9874563210"},
+    {id:1005, firstName:"vamsi", middleName:"middleNameOne2", lastName:"lastName12", email:"User12.lastname12@gmail.com", mobileNumber:"12345678902"},
+    {id:1006, firstName:"Charan", middleName:"middleNameTwo2", lastName:"lastName22", email:"User22.lastname22@gmail.com", mobileNumber:"12365897422"},
+    {id:1007, firstName:"RANJITH3", middleName:"", lastName:"Nagavelli3", email:"ranjith3.nagavelli3@gmail.com", mobileNumber:"98745632103"},
+    {id:1008, firstName:"Chiru", middleName:"middleNameOne3", lastName:"lastName13", email:"User13.lastname1@gmail.com", mobileNumber:"12345678903"},
+    {id:1009, firstName:"Pawan", middleName:"middleNameTwo3", lastName:"lastName23", email:"User23.lastname2@gmail.com", mobileNumber:"12365897423"}
   ];
   private lastindex:number = 1009;
 
